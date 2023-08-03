eac5ef322fe96d75f11c213d5183b05f6f861bb5
jacklv111 <lv523954325@163.com>
2023-08-03 17:23:58 +0800

    .

--- cmd/test/k8s-client/main.go
+++ cmd/test/k8s-client/main.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 
 	"github.com/google/uuid"
+	"github.com/jacklv111/common-sdk/log"
 	"github.com/jacklv111/common-sdk/scheduler"
 	"github.com/jacklv111/optimus/infra/client/k8s"
 	corev1 "k8s.io/api/core/v1"
@@ -55,8 +56,11 @@ func main() {
 		fmt.Println(isCompleted, err)
 		return k8s.IsJobCompleted(namespace, jobName)
 	})
+	log.Infof("delete %s:%s", namespace, jobName)
 	err = k8s.DeleteJob(namespace, jobName)
 	if err != nil {
 		fmt.Printf("delete job error: %s", err)
+	} else {
+		fmt.Println("delete job success")
 	}
 }
