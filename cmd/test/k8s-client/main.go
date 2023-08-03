aa6327b3c4e5d6e67dc8ddc51ead06547f5dbb2c
jacklv111 <lv523954325@163.com>
2023-08-03 16:43:41 +0800

    .

--- cmd/test/k8s-client/main.go
+++ cmd/test/k8s-client/main.go
@@ -11,20 +11,21 @@ import (
 	"context"
 	"fmt"
 
+	"github.com/google/uuid"
+	"github.com/jacklv111/common-sdk/scheduler"
 	"github.com/jacklv111/optimus/infra/client/k8s"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func main() {
-	k8s.K8sConfig.ApiServerUrl = "https://192.168.0.185:5443"
 
 	err := k8s.InitK8sClient()
 	if err != nil {
 		fmt.Printf("k8s init error: %s", err)
 	}
 	namespace := "dev"
-	jobName := "hello-world"
+	jobName := "hello-world" + uuid.New().String()[0:5]
 	// Create a Job object
 	job := &corev1.Pod{
 		ObjectMeta: metav1.ObjectMeta{
@@ -46,5 +47,15 @@ func main() {
 	_, err = k8s.Clientset.CoreV1().Pods(namespace).Create(context.TODO(), job, metav1.CreateOptions{})
 	if err != nil {
 		fmt.Printf("create job error: %s", err)
+	}
+	// wait for action finished
+	scheduler.WaitCondition(3, func() bool {
+		isCompleted := k8s.IsJobCompleted(namespace, jobName)
+		fmt.Println(isCompleted, err)
+		return isCompleted
+	})
+	err = k8s.DeleteJob(namespace, jobName)
+	if err != nil {
+		fmt.Printf("delete job error: %s", err)
 	}
 }
