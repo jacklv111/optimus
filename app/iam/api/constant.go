abb22a3cec1dec8058226381872f585b4004d69c
jacklv111 <lv523954325@163.com>
2023-08-04 00:53:04 +0800

    .

---
+++ app/iam/api/constant.go
@@ -0,0 +1,12 @@
+/*
+ * Created on Fri Aug 04 2023
+ *
+ * Copyright (c) 2023 Company-placeholder. All rights reserved.
+ *
+ * Author Yubinlv.
+ */
+package api
+
+const (
+	DEFAULT_WORKSPACE = "default"
+)
